52216201399edd60eafcff77f8ccbb133b322e8c
DWAYNE JONES <Dwaynekj@gmail.com>
2021-12-08 01:20:07 -0500

    Updated script

--- scripts/google_upload_data.sh
+++ scripts/google_upload_data.sh
@@ -4,7 +4,7 @@ pushd $( dirname "${BASH_SOURCE[0]}" ) >/dev/null 2>&1
 
 read -p "please enter your project id: " GC_PROJECT_ID
 
-gsutil -m cp ../data/matchweek/* gs://${GC_PROJECT_ID} && \
-gsutil -m cp ../data/scorer/* gs://${GC_PROJECT_ID}
+gsutil cp ../data/matchweek/* gs://${GC_PROJECT_ID} && \
+gsutil cp ../data/scorer/* gs://${GC_PROJECT_ID}
 
 popd
